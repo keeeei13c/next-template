b1786d3522f25b6b1ca9867a5e8e43a95f538d95
keeeei13c <kei.121003f@icloud.com>
2022-03-19 17:43:15 +0900

    recoilのtutorialをReactのみで書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 
+const Home = () => {
+  const [text, setText] = useState("");
 
-const Home= () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <input
+        type="text"
+        title="text"
+        onChange={handleChange}
+        value={text}
+        placeholder={text}
+      />
+      <p>{text ? text : "なし"}</p>
+      <p>文字数{text.length}</p>
+    </div>
+  );
 };
 
 export default Home;
